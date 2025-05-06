19c23c16cbaeab740b4f182e696d9da33b3119c7
Jacob C <jacob.j.carlson03@gmail.com>
2025-05-06 10:51:58 -0700

    prof class finding is working well and shows real results

--- professors/find-classes.php
+++ professors/find-classes.php
@@ -20,17 +20,47 @@
     if (!$link) {
         die('Could not connect: ' . mysql_error());
     }
-    echo 'Connected successfully<p>';
-
-    printf("<h3>Results for SSN %s:</h3>\n", $_POST["ssn"]);
-
-    // **** THIS IS A PLACEHOLDER QUERY. UPDATE IT AND REMOVE THIS COMMENT ****/
-    printf("THIS IS A PLACEHOLDER QUERY. UPDATE IT AND REMOVE THIS MESSAGE.<br>\n");
-    $query = "SELECT * FROM Student WHERE ZipCode=" . $_POST["ssn"];
-    $result = $link->query($query);
-    $row = $result->fetch_assoc();
-    printf("ZIP: %s<br>\n", $row["ZipCode"]);
-    printf("Name: %s %s<br>\n", $row["FirstName"], $row["LastName"]);
+    printf("<h3>Results for SSN ending in %s:</h3>\n", substr($_POST["ssn"], -4));
+    $query = "SELECT C.Title, S.Classroom, S.MeetingDays, S.BeginTime, S.EndTime
+        FROM Professor AS P
+        JOIN Section AS S ON P.SSN = S.ProfSSN AND P.SSN= ? 
+        JOIN Course AS C ON C.CourseNumber = S.CourseNumber;";
+    $statement = $link->prepare($query);
+    if (!$statement)
+    {
+        echo "Error: " . $link->error;
+        return;
+    }
+    $statement->bind_param("s", $_POST["ssn"]);
+    $statement->execute();
+    $result = $statement->get_result();
+    echo "<h3>" . $result->num_rows . " classes found.</h3>";
+    echo "<table border='1' cellpadding='5' cellspacing='0'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>Class title</th>";
+    echo "<th>Room</th>";
+    echo "<th>Meeting days</th>";
+    echo "<th>Begin time</th>";
+    echo "<th>End time</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    while($row = $result->fetch_assoc())
+    {
+        echo "<tr>";
+        echo "<td>" . $row["Title"] . "</td>";
+        echo "<td>" . $row["Classroom"] . "</td>";
+        echo "<td>" . $row["MeetingDays"] . "</td>";
+        $beginTime = sprintf("%02d:%02d", intdiv($row["BeginTime"], 100), $row["BeginTime"] % 100);
+        echo "<td>" . $beginTime . "</td>";
+        $endTime = sprintf("%02d:%02d", intdiv($row["EndTime"], 100), $row["EndTime"] % 100);
+        echo "<td>" . $endTime . "</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+    $statement->close();
     $result->free_result();
     $link->close();
     ?>
